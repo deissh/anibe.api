0d9e16ff6ab5232e48e0cb48c98363879e7b581e
deissh <kudryavtsev@nppsatek.ru>
2019-02-14 22:10:22 +0300

    подправил тесты + добавил тесты пользовательских списков

--- src/api/post/model.test.js
+++ src/api/post/model.test.js
@@ -3,7 +3,27 @@ import { Post } from '.';
 let post;
 
 beforeEach(async () => {
-  post = await Post.create({ name: 'test', annotation: 'test', description: 'test', genre: 'test', type: 'test', rating: 'test', status: 'test', date: 'test', author: 'test', cover: 'test', chapters: 'test', pages: 'test', reading: 'test', episodes: 'test' });
+  post = await Post.create({
+    name: 'test',
+    annotation: 'test',
+    description: 'test',
+    genre: [ 'test1', 'test2' ],
+    type: 'test',
+    rating: 5.1,
+    status: 'test',
+    date: 'test',
+    author: 'test',
+    cover: 'test',
+    chapters: 'test',
+    pages: 'test',
+    reading: 'test',
+    episodes: {
+      1: [
+        'first',
+        'second'
+      ]
+    }
+  });
 });
 
 describe('view', () => {
@@ -11,13 +31,12 @@ describe('view', () => {
   it('returns simple view', () => {
     const view = post.view();
     expect(typeof view).toBe('object');
-    expect(view.id).toBe(post.id);
-    expect(view.name).toBe(post.name);
-    expect(view.annotation).toBe(post.annotation);
-    expect(view.genre).toBe(post.genre);
-    expect(view.type).toBe(post.type);
-    expect(view.rating).toBe(post.rating);
-    expect(view.cover).toBe(post.cover);
+    expect(view.name).toEqual('test');
+    expect(view.annotation).toEqual('test');
+    expect(view.type).toEqual('test');
+    expect(view.rating).toEqual(5.1);
+    expect(view.author).toEqual('test');
+    expect(view.cover).toEqual('test');
   });
 
   it('returns full view', () => {
