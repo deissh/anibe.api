089133b4e3c66325eb7147c80cc7f13e8fa9d3f8
deissh <kudryavtsev@nppsatek.ru>
2019-02-19 21:49:51 +0300

    add pm2 monit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const io = require('@pm2/io');
+
+io.init({
+  transactions: true, // will enable the transaction tracing
+  http: true // will enable metrics about the http server (optional)
+});
+
 require('babel-core/register');
 
 exports = module.exports = require('./app');
