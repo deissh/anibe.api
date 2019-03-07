9a32f989be44dbf7296b13351c5b996d9139714b
deissh <kudryavtsev@nppsatek.ru>
2019-03-07 20:36:22 +0300

    исправил получение новости по айди

--- src/api/news/controller.js
+++ src/api/news/controller.js
@@ -21,7 +21,7 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
 export const show = ({ params }, res, next) =>
   News.findById(params.id)
     .then(notFound(res))
-    .then((news) => news ? news.view() : null)
+    .then((news) => news ? news.view(true) : null)
     .then(success(res))
     .catch(next);
 
