e7fff47a9bc9a478dded443883ef72f4192f17d7
deissh <kudryavtsev@nppsatek.ru>
2019-03-18 21:05:41 +0300

    подправил код + добавил redis

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-const io = require('@pm2/io');
-
-io.init({
-  transactions: true, // will enable the transaction tracing
-  metrics: {
-    network: {
-      ports: true
-    },
-    http: true
-  }
-});
-
 require('babel-core/register');
 
 exports = module.exports = require('./app');
